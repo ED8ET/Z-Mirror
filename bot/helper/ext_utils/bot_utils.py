60468cae6476f168ad292cad18480c760e9b705b
ED8ET <121234233+ED8ET@users.noreply.github.com>
2023-07-05 15:15:19 +0630

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -167,7 +167,7 @@ dispatcher.add_handler(CallbackQueryHandler(pop_up_stats, pattern=f"^{str(THREE)
 
 def get_readable_message():
     with download_dict_lock:
-        msg = f'<a href="https://t.me/h2dts">          🇯🇵<b>Source Please</b>🇯🇵      </a>\n'
+        msg = f'<a href="https://t.me/h2dts">                   🇯🇵<b>Source Please</b>🇯🇵      </a>\n'
         if STATUS_LIMIT is not None:
             tasks = len(download_dict)
             globals()['PAGES'] = ceil(tasks/STATUS_LIMIT)
