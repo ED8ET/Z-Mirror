7944abce8bfa57c6cf54b0380e92013f163ab027
ED8ET <121234233+ED8ET@users.noreply.github.com>
2023-07-05 14:27:37 +0630

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -167,7 +167,7 @@ dispatcher.add_handler(CallbackQueryHandler(pop_up_stats, pattern=f"^{str(THREE)
 
 def get_readable_message():
     with download_dict_lock:
-        msg = ""
+        msg = f'<a href="https://t.me/mirrorbd"><b>mirrorBD MLTB</b></a>\n\n'
         if STATUS_LIMIT is not None:
             tasks = len(download_dict)
             globals()['PAGES'] = ceil(tasks/STATUS_LIMIT)
